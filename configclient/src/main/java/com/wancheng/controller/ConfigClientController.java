04a790ff4f6c51048e7c73c8403f5414b8900aae
liutiezhu <m18813933843@163.com>
2020-05-24 21:50:51 +0800

    远程获取配置文件

---
+++ configclient/src/main/java/com/wancheng/controller/ConfigClientController.java
@@ -0,0 +1,19 @@
+package com.wancheng.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    String port;
+
+    @GetMapping("index")
+    String index(){
+        return port;
+    }
+}
